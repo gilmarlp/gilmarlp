bee181a7b8f5f9caa5e8fc35130d80587d3baa2f
Gilmar Lima Pelegrini <gilmarpjr93@gmail.com>
2025-09-24 10:41:32 -0300

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,14 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }),
 ];
 
 export default eslintConfig;
